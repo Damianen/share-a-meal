b4fab3cbc0e640c05c93095b8b1b3d760147c2be
Damian <113175544+Damianen@users.noreply.github.com>
2024-04-30 18:54:01 +0200

    Fix bug where nodemon is not loaded properly

--- src/dtb/inmem-db.js
+++ src/dtb/inmem-db.js
@@ -1,68 +1,43 @@
-//
-// Onze lokale 'in memory database'.
-// We simuleren een asynchrone database met een array van objecten.
-// De array bevat een aantal dummy records.
-// De database heeft twee methoden: get en add.
-// Opdracht: Voeg de overige methoden toe.
-//
 const database = {
-    // het array met dummy records. Dit is de 'database'.
     _data: [
         {
             id: 0,
             firstName: 'Hendrik',
             lastName: 'van Dam',
             emailAdress: 'hvd@server.nl'
-            // Hier de overige velden uit het functioneel ontwerp
         },
         {
             id: 1,
             firstName: 'Marieke',
             lastName: 'Jansen',
             emailAdress: 'm@server.nl'
-            // Hier de overige velden uit het functioneel ontwerp
         }
     ],
-
-    // Ieder nieuw item in db krijgt 'autoincrement' index.
-    // Je moet die wel zelf toevoegen aan ieder nieuw item.
     _index: 2,
 
     getAll(callback) {
-        // Simuleer een asynchrone operatie
         setTimeout(() => {
-            // Roep de callback aan, en retourneer de data
-            callback(null, this._data)
-        }, 1500)
+            callback(null, this._data);
+        }, 1500);
     },
 
     getById(id, callback) {
-        // Simuleer een asynchrone operatie
         setTimeout(() => {
             if (id < 0 || id >= this._data.length) {
-                callback({ message: `Error: id ${id} does not exist!` }, null)
+                callback({ message: `Error: id ${id} does not exist!` }, null);
             } else {
-                callback(null, this._data[id])
+                callback(null, this._data[id]);
             }
         }, 1500)
     },
 
     add(item, callback) {
-        // Simuleer een asynchrone operatie
         setTimeout(() => {
-            // Voeg een id toe en voeg het item toe aan de database
-            item.id = this._index++
-            // Voeg item toe aan de array
-            this._data.push(item)
-
-            // Roep de callback aan het einde van de operatie
-            // met het toegevoegde item als argument, of null als er een fout is opgetreden
-            callback(null, item)
+            item.id = this._index++;
+            this._data.push(item);
+            callback(null, item);
         }, 1500)
     }
-
-    // Voeg zelf de overige database functionaliteit toe
 }
 
 export default database;
-// module.exports = database.index;
